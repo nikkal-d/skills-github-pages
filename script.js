7f520d1684bf61187cd0a150cb5dfa98fa38f445
nikkal-d <akirotato2@gmail.com>
2026-03-11 23:35:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-                    // ΡΟΛΟΙ //
+// ΡΟΛΟΙ //
 
 let wra = document.getElementById('wra');
 let lepta = document.getElementById('lepta');
 let deyt = document.getElementById('deyt');
 
-setInterval(()=>{
+setInterval(() => {
+    let currentTime = new Date();
+
+    // Χρησιμοποιούμε παρενθέσεις για να γίνει σωστά η πρόσθεση του "0" με τον αριθμό
+    wra.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    lepta.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    deyt.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
     
-let currentTime = new Date();
-   
-wra.innerHTML = currentTime.getHours()<10?"0":"" + currentTime.getHours();
-lepta.innerHTML = currentTime.getMinutes()<10?"0":"" + currentTime.getMinutes();
-deyt.innerHTML = currentTime.getSeconds()<10?"0":"" + currentTime.getSeconds();
+    // Αν θέλεις να βλέπεις την ώρα στην κονσόλα, πρέπει να το βάλεις ΕΔΩ ΜΕΣΑ:
+    // console.log(currentTime); 
 
-},1000);
+}, 1000);
 
-console.log(currentTime);
+// Διέγραψε ή βάλε σε σχόλιο την παρακάτω γραμμή, γιατί προκαλεί το Error:
+// console.log(currentTime);
