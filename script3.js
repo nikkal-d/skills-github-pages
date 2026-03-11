9be8ef763d83d2f8c571eac1b200a24eb48f34e7
nikkal-d <akirotato2@gmail.com>
2026-03-11 23:40:09 +0200

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,13 +1,15 @@
-// SLIDE //
-let epomeni = document.querySelector('.epomeni');
+                //SLIDE //
+
+
+
+let epomeni = document.querySelector('.epomeni')
+
+
 
 epomeni.addEventListener('click', function(){
-    // Επιλέγουμε όλα τα στοιχεία με την κλάση .imag
-    let items = document.querySelectorAll('.imag');
-    
-    // Έλεγχος αν υπάρχουν στοιχεία για να μην βγάζει error
-    if (items.length > 0) {
-        // Παίρνει το πρώτο στοιχείο [0] και το βάζει στο τέλος της λίστας
-        document.querySelector('.slide').appendChild(items[0]);
-    }
-});
+
+    let items = document.querySelectorAll('.imag')
+
+    document.querySelector('.slide').appendChild(items[0])
+
+}) 
